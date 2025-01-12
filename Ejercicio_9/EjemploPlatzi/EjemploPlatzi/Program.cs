ccf4fcb52788db51c1d7380f69b3715c61e437e5
GDieser <germandieser@gmail.com>
2025-01-11 21:31:51 -0300

    Practica de POO

---
+++ Ejercicio_9/EjemploPlatzi/EjemploPlatzi/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Mouse mouse = new Mouse();
+
+mouse.material = "Plastico";
+mouse.color = "Gris";
+
+class Mouse
+{
+    //Propiedades
+    public string color;
+    public string material;
+    public string marca;
+    public double tamanio;
+    public int botones;
+
+}
